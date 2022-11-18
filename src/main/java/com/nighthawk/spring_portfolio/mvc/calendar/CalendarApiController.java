17d0eb9163e9b632d7de31b688f15a98f010d805
Don Tran <donqt15@gmail.com>
2022-11-17 21:32:31 -0800

    finished first 3 APIs

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
@@ -11,30 +11,63 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/** Calendar API
- * Calendar Endpoint: /api/calendar/isLeapYear/2022, Returns: {"year":2020,"isLeapYear":false}
+/**
+ * Calendar API
+ * Calendar Endpoint: /api/calendar/isLeapYear/2022, Returns:
+ * {"year":2020,"isLeapYear":false}
  */
 @RestController
 @RequestMapping("/api/calendar")
 public class CalendarApiController {
 
-    /** GET isLeapYear endpoint
-     * ObjectMapper throws exceptions on bad JSON
-     *  @throws JsonProcessingException
-     *  @throws JsonMappingException
-     */
-    @GetMapping("/isLeapYear/{year}")
-    public ResponseEntity<JsonNode> getIsLeapYear(@PathVariable int year) throws JsonMappingException, JsonProcessingException {
-      // Backend Year Object
-      Year year_obj = new Year();
-      year_obj.setYear(year);  // evaluates Leap Year
+  /**
+   * GET isLeapYear endpoint
+   * ObjectMapper throws exceptions on bad JSON
+   * 
+   * @throws JsonProcessingException
+   * @throws JsonMappingException
+   */
+  @GetMapping("/isLeapYear/{year}")
+  public ResponseEntity<JsonNode> getIsLeapYear(@PathVariable int year)
+      throws JsonMappingException, JsonProcessingException {
+    // Backend Year Object
+    Year year_obj = new Year();
+    year_obj.setYear(year); // evaluates Leap Year
 
-      // Turn Year Object into JSON
-      ObjectMapper mapper = new ObjectMapper(); 
-      JsonNode json = mapper.readTree(year_obj.isLeapYearToString()); // this requires exception handling
+    // Turn Year Object into JSON
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode json = mapper.readTree(year_obj.isLeapYearToString()); // this requires exception handling
 
-      return ResponseEntity.ok(json);  // JSON response, see ExceptionHandlerAdvice for throws
-    }
+    return ResponseEntity.ok(json); // JSON response, see ExceptionHandlerAdvice for throws
+  }
 
-    // add other methods
+  // add other methods
+  @GetMapping("/firstDayOfYear/{year}")
+  public ResponseEntity<JsonNode> getFirstDayOfYear(@PathVariable int year)
+      throws JsonMappingException, JsonProcessingException {
+    // Backend Year Object
+    Year year_obj = new Year();
+    year_obj.setYear(year); // evaluates Leap Year
+
+    // Turn Year Object into JSON
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode json = mapper.readTree(year_obj.firstDayOfYearToString()); // this requires exception handling
+
+    return ResponseEntity.ok(json); // JSON response, see ExceptionHandlerAdvice for throws
+  }
+
+  @GetMapping("/dayOfYear/{month}/{day}/{year}")
+  public ResponseEntity<JsonNode> getDayOfYear(@PathVariable int month, @PathVariable int day, @PathVariable int year)
+      throws JsonMappingException, JsonProcessingException {
+    // Backend Year Object
+    Year year_obj = new Year();
+    year_obj.setDate(month, day, year); // evaluates Leap Year
+
+    // Turn Year Object into JSON
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode json = mapper.readTree(year_obj.dayOfYearToString()); // this requires exception handling
+
+    return ResponseEntity.ok(json); // JSON response, see ExceptionHandlerAdvice for throws
+  }
+
 }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
@@ -1,47 +1,101 @@
 package com.nighthawk.spring_portfolio.mvc.calendar;
 
-/** Simple POJO 
+/**
+ * Simple POJO
  * Used to Interface with APCalendar
  * The toString method(s) prepares object for JSON serialization
  * Note... this is NOT an entity, just an abstraction
  */
 class Year {
    private int year;
+   private int month;
+   private int day;
    private boolean isLeapYear;
+   private int firstDayOfYear;
+   private int dayOfYear;
+   private int numberOfLeapYears;
 
    // zero argument constructor
-   public Year() {} 
+   public Year() {
+   }
 
    /* year getter/setters */
    public int getYear() {
       return year;
    }
+
    public void setYear(int year) {
       this.year = year;
       this.setIsLeapYear(year);
+      this.setFirstDayOfYear(year);
+   }
+
+   /* date getter/setters */
+   public int getDate() {
+      return year;
+   }
+
+   public void setDate(int month, int day, int year) {
+      this.month = month;
+      this.day = day;
+      this.year = year;
+      this.setIsLeapYear(year);
+      this.setFirstDayOfYear(year);
+      this.setDayOfYear(month, day, year);
    }
 
    /* isLeapYear getter/setters */
    public boolean getIsLeapYear(int year) {
       return APCalendar.isLeapYear(year);
    }
-   private void setIsLeapYear(int year) {  // this is private to avoid tampering
+
+   private void setIsLeapYear(int year) { // this is private to avoid tampering
       this.isLeapYear = APCalendar.isLeapYear(year);
    }
 
    /* isLeapYearToString formatted to be mapped to JSON */
-   public String isLeapYearToString(){
-      return ( "{ \"year\": "  +this.year+  ", " + "\"isLeapYear\": "  +this.isLeapYear+ " }" );
-   }	
+   public String isLeapYearToString() {
+      return ("{ \"year\": " + this.year + ", " + "\"isLeapYear\": " + this.isLeapYear + " }");
+   }
+
+   /* firstDayOfYear getter/setters */
+   public int getFirstDayOfYear(int year) {
+      return APCalendar.firstDayOfYear(year);
+   }
+
+   private void setFirstDayOfYear(int year) { // this is private to avoid tampering
+      this.firstDayOfYear = APCalendar.firstDayOfYear(year);
+   }
+
+   /* firstDayOfYearToString formatted to be mapped to JSON */
+   public String firstDayOfYearToString() {
+      return ("{ \"year\": " + this.year + ", " + "\"firstDayOfYear\": " + this.firstDayOfYear + " }");
+   }
+
+   /* dayOfYear getter/setters */
+   public int getDayOfYear(int month, int day, int year) {
+      return APCalendar.dayOfYear(month, day, year);
+   }
+
+   private void setDayOfYear(int month, int day, int year) { // this is private to avoid tampering
+      this.dayOfYear = APCalendar.dayOfYear(month, day, year);
+   }
+
+   /* firstDayOfYearToString formatted to be mapped to JSON */
+   public String dayOfYearToString() {
+      return ("{ \"month\": " + this.month + ", " + "\"day\": " + this.day + ", " + "\"year\": " + this.year + ", "
+            + "\"dayOfYear\": " + this.dayOfYear + " }");
+   }
 
    /* standard toString placeholder until class is extended */
-   public String toString() { 
-      return isLeapYearToString(); 
+   public String toString() {
+      return dayOfYearToString();
    }
 
    public static void main(String[] args) {
       Year year = new Year();
-      year.setYear(2022);
+      // year.setYear(2022);
+      year.setDate(3, 4, 2028);
       System.out.println(year);
    }
 }
