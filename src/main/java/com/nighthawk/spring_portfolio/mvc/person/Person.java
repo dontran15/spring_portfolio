315bc5e6c7ec14431498a472b718cde0b4e3440d
Don Tran <donqt15@gmail.com>
2022-11-24 23:59:18 -0800

    new params for person object & person unit test

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.person;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
@@ -38,9 +39,9 @@ The last annotation connect to database
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@TypeDef(name="json", typeClass = JsonType.class)
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class Person {
-    
+
     // automatic unique identifier for Person record
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -48,50 +49,95 @@ public class Person {
 
     // email, password, roles are key attributes to login and authentication
     @NotEmpty
-    @Size(min=5)
-    @Column(unique=true)
+    @Size(min = 5)
+    @Column(unique = true)
     @Email
     private String email;
 
     @NotEmpty
     private String password;
 
-    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
+    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max =
+    // 30, message = "Name (2 to 30 chars)") String name"
     @NonNull
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
-    
 
-    /* HashMap is used to store JSON for daily "stats"
-    "stats": {
-        "2022-11-13": {
-            "calories": 2200,
-            "steps": 8000
-        }
-    }
-    */
-    @Type(type="json")
+    // health related info (other than age)
+    private double height; // in inches
+    private double weight; // in lb
+    private String gender;
+
+    /*
+     * HashMap is used to store JSON for daily "stats"
+     * "stats": {
+     * "2022-11-13": {
+     * "calories": 2200,
+     * "steps": 8000
+     * }
+     * }
+     */
+    @Type(type = "json")
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
 
     // Constructor used when building object from an API
-    public Person(String email, String password, String name, Date dob) {
+    public Person(String email, String password, String name, Date dob, double height, double weight, String gender) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender.toLowerCase();
     }
 
     // A custom getter to return age from dob attribute
     public int getAge() {
         if (this.dob != null) {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
+    }
+
+    // Calories required to burn per day to maintain current weight (Uses Active &
+    // Basal
+    // Metabolic Rate)
+    public int getCaloriesRequiredPerDay() {
+        if (this.dob != null && this.weight != 0 && this.height != 0) {
+            double BMR = 66.5 + (13.75 * weight / 2.205) + (5.003 * this.height * 2.54) - (6.75 * getAge());
+            double AMR = BMR * 1.55; // assumes moderate exercise each day, indicates calories needed to burn
+            return (int) Math.round(AMR - BMR);
+        }
+
+        return -1;
+    }
+
+    // toString Method
+    public String toString() {
+        return ("{ \"email\": " + this.email + ", " + "\"password\": " + this.password + ", " + "\"name\": " + this.name
+                + ", " + "\"dob\": " + this.dob + ", " + "\"height\": " + this.height + ", " + "\"weight\": "
+                + this.weight + ", " + "\"gender\": " + this.gender + " }");
+    }
+
+    // Unit Test
+    public static void main(String[] args) {
+        // instantiates no args & all args
+        Person personNoArgs = new Person();
+        Person personAllArgs = new Person("someone@email.com", "12345", "Joe Jack",
+                new java.util.GregorianCalendar(2000, 0, 1).getTime(), 70, 165, "Male");
+
+        // toString prints
+        System.out.println(personNoArgs.toString());
+        System.out.println(personAllArgs.toString());
+
+        // getCaloriesRequiredPerDay print tests
+        System.out.println(personNoArgs.getCaloriesRequiredPerDay());
+        System.out.println(personAllArgs.getCaloriesRequiredPerDay());
     }
 
 }
