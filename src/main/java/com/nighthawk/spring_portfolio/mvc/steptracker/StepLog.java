6ee60f18143230aecb95fb13cfb2937a6409dd26
Don Tran <donqt15@gmail.com>
2022-11-26 18:29:57 -0800

    steplog constructor, TODO: ManyToOne

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptracker/StepLog.java
@@ -0,0 +1,55 @@
+package com.nighthawk.spring_portfolio.mvc.steptracker;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class StepLog {
+    // automatic unique identifier for stepLog record
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    @Positive
+    private int steps;
+
+    public StepLog(Date date, int steps) {
+        this.date = date;
+        this.steps = steps;
+    }
+}
